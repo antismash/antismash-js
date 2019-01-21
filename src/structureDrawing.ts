a724cf6bf650d728677dfeaf36e983eb1ed130ab
Simon Shaw <SJShaw@users.noreply.github.com>
2019-01-21 18:08:39 +0100

    structureDrawing: draw expanded modals on click

--- src/structureDrawing.ts
+++ src/structureDrawing.ts
@@ -6,6 +6,7 @@ import SmilesDrawer from "smiles-drawer";
 export function drawStructure(this: HTMLElement): void {
     const options = {
         height: 200,
+        padding: 30,
         width: ($(this).parent().width() || 270) - 20,
     };
     // SmilesDrawer can't cope with drawing while hidden, so display
@@ -21,8 +22,35 @@ export function drawStructure(this: HTMLElement): void {
     if (!expanded) {
         $(this).parent().removeAttr("style");
     }
+    $(this).click(function(this: HTMLElement, event: JQuery.Event) {
+        $(`#${$(this).attr("id")}-modal`).show();
+        const target = $(`#${$(this).attr("id")}-modal`).children(".modal-content").children(".smiles-canvas-modal").first();
+        if (typeof(target) === "undefined") {
+            return;
+        }
+        const modalOptions = {
+            height: (target.height() || 500),
+            padding: 30,
+            width: ((target.width() || 700) + 80),  // unintuitive, but forces fitting into element
+        };
+        // again, expand it first
+        const modalExpanded: boolean = target.parent().parent().hasClass("expanded");
+        if (!modalExpanded) {
+            target.parent().css("display", "block");
+        }
+        const modalSmilesDrawer = new SmilesDrawer.Drawer(modalOptions);
+        const modalCanvas: HTMLCanvasElement = target[0] as HTMLCanvasElement;
+        SmilesDrawer.parse(modalCanvas.getAttribute("data-smiles"),
+                           (tree: string) => modalSmilesDrawer.draw(tree, modalCanvas, "light", false));
+        if (!modalExpanded) {
+            $(target).parent().removeAttr("style");
+        }
+        $(`#${$(this).attr("id")}-modal`).children(".modal-content")
+            .css("top", `${$(this).position().top - modalOptions.height / 2}px`);
+    });
 }
 
 export function drawStructures(): void {
   $(".smiles-canvas").each(drawStructure);
+  $(".modal-container").off("click").click(() => $(".modal-container").hide());
 }
