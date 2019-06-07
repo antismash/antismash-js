50ccd679cbe96c0f3242c0fd1499aac0b6bd54f8
Simon Shaw <SJShaw@users.noreply.github.com>
2019-06-07 11:00:05 +0200

    structureDrawing: use new experimental mode in smiles-drawer

--- src/structureDrawing.ts
+++ src/structureDrawing.ts
@@ -6,6 +6,7 @@ import SmilesDrawer from "smiles-drawer";
 export function drawStructure(this: HTMLElement): void {
     const canvas: HTMLCanvasElement = this as HTMLCanvasElement;
     const options = {
+        experimentalSSSR: true,
         height: 200,
         padding: 30,
         width: ($(canvas).parent().width() || 270) - 20,
@@ -33,6 +34,7 @@ export function drawStructure(this: HTMLElement): void {
             return;
         }
         const modalOptions = {
+            experimentalSSSR: true,
             height: (target.height() || 500),
             padding: 30,
             width: ((target.width() || 700) + 80),  // unintuitive, but forces fitting into element
