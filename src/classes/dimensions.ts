2ba5cc7cfd8689eca7a5cd0d1269561407b72c8a
Simon Shaw <SJShaw@users.noreply.github.com>
2024-10-10 13:40:23 +0200

    classes: add a container for dimensions

---
+++ src/classes/dimensions.ts
@@ -0,0 +1,11 @@
+/* License: GNU Affero General Public License v3 or later
+   A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt. */
+
+export class Dimensions {
+    public height: number;
+    public width: number;
+    constructor(width: number, height: number) {
+        this.height = height;
+        this.width = width;
+    }
+}
