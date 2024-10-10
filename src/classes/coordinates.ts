22806edc77ebc63201bdaa83a01e24a19af806de
Simon Shaw <SJShaw@users.noreply.github.com>
2024-10-10 13:33:22 +0200

    classes: add helper class for storing coordinates

---
+++ src/classes/coordinates.ts
@@ -0,0 +1,12 @@
+/* License: GNU Affero General Public License v3 or later
+   A copy of GNU AGPL v3 should have been included in this software package in LICENSE.txt. */
+
+export class Coordinates {
+    public x: number;
+    public y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+}
